3e7c8b12f23c98207437622c4216ce811a8217f1
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-02-13 21:39:02 +0100

    dit werkt ook een beetje

--- controller.php
+++ controller.php
@@ -7,19 +7,20 @@ require 'Blackjack.php';
 session_start();
 
 
-var_dump( $player ?? "nuttin");
+var_dump($player ?? "nuttin");
 
 
-
-
+if (!(isset($_POST['submit']))) {
+    $_POST['submit'] = "";
+}
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (!(isset($_POST['submit']))) {
-        $_POST['submit'] = "";
-    }
-
+//    if (!(isset($_POST['submit']))) {
+//        $_POST['submit'] = "";
+//    }
+    // HIT ME
     if ($_POST['submit'] == 0) {
         if (!isset($_SESSION['player'])) {
             $player = new Blackjack();
@@ -29,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         $_SESSION['player']->hit();
 
-
+        // STAND
     } else if ($_POST['submit'] == 1) {
         if (!isset($_SESSION['player'])) {
             $player = new Blackjack();
@@ -46,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $dealer->hit();
         } while ($dealer->totalValue < $player->totalValue);
 
-
+        // SURRENDER
     } else if ($_POST['submit'] == 2) {
         if (!isset($_SESSION['player'])) {
             $player = new Blackjack();
@@ -57,4 +58,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     }
 
+} else {
+    if (!isset($_SESSION['player'])) {
+        $player = new Blackjack();
+        $_SESSION['player'] = $player;
+    } else {
+        $player = $_SESSION['player'];
+    }
 }
